a1e94d1f6baf99b94d472e6b6c31327ffb300041
Moonwalkert3ch <6620@holbertonstudents.com>
2024-09-17 15:59:10 -0500

    footer renders correctly and alerts

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,7 @@
 import { Provider } from "react-redux";
 import { store } from "../store";
 import { Header } from "./Header";
+import { Footer } from "./Footer";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
     <Provider store={store}>
       <Header />
     Hello Atlas
+    <Footer />
     </Provider>
   );
 }
