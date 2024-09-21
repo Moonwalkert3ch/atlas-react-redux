fccde3caaa8b2c40fecfd8618ec7f9c9067f82d3
Moonwalkert3ch <6620@holbertonstudents.com>
2024-09-21 18:33:18 -0500

    put the delete card button in a component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,10 +2,6 @@ import React from "react";
 // import { DeleteListButton } from "./DeleteListButton";
 
 export const Card: React.FC = () => {
-  const handleDelete = () => {
-    alert("Delete card")
-  };
-
   return(
       <div className="card group/card m-3 flex min-h-24 flex-col items-start rounded bg-off-white-light px-4 py-2 text-blue">
         <h5 className="my-2 flex w-full items-end justify-between text-xl font-black">
@@ -16,5 +12,5 @@ export const Card: React.FC = () => {
             Sed viverra, diam eu facilisis bibendum, ante orci placerat quam
           </p>
         </div>
-    )
+  )
 }
