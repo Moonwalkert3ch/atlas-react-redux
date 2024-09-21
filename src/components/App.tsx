ed5b29abf999b983215d9274c3991f80aafd0e40
Moonwalkert3ch <6620@holbertonstudents.com>
2024-09-21 18:25:00 -0500

    made the card component, delete button renders on hover and removed w-full to change width from being full screen

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,6 +2,7 @@ import { Provider } from "react-redux";
 import { store } from "../store";
 import { Header } from "./Header";
 import { Footer } from "./Footer";
+import { Card } from "./Card";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
     <Provider store={store}>
       <Header />
     Hello Atlas
+    <Card />
     <Footer />
     </Provider>
   );
